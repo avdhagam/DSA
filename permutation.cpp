3f27b95ac0cb30cb8dea1b88d5b8fb0caf4b01ef
avdhagam <davanisri@gmail.com>
2024-03-10 18:29:24 +0530

    added permutation using recursion program (swap approach)

---
+++ permutation.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#include <vector>
+
+void recurPermute(int index, vector<vector<int>> &ans, vector<int> &nums);
+
+vector<vector<int>> permute(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    recurPermute(0, ans, nums);
+    return ans;
+}
+
+void recurPermute(int index, vector<vector<int>> &ans, vector<int> &nums)
+{
+    if (index == nums.size())
+    {
+        ans.push_back(nums);
+        return;
+    }
+    for (int i = index; i < nums.size(); i++)
+    {
+        swap(nums[i], nums[index]);
+        recurPermute(index + 1, ans, nums);
+        swap(nums[i], nums[index]);
+    }
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3};
+    vector<vector<int>> ans = permute(v);
+    for (auto i : ans)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
