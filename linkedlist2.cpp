2be18b083c0de3d5cf3a27598dce56663fd1f496
avdhagam <davanisri@gmail.com>
2024-03-18 20:22:09 +0530

    added program to traverse and find the length of a linked list

---
+++ linkedlist2.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+using namespace std;
+#include <iostream>
+#include <vector>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = NULL;
+    }
+};
+
+Node *ArrayToLL(vector<int> arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+int lengthofLL(Node *head)
+{
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+    Node *head = ArrayToLL(v);
+    cout << lengthofLL(head);
+}
