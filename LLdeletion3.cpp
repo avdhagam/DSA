cca85123c9d76c3b03beceed089d3d4c9628f225
avdhagam <davanisri@gmail.com>
2024-03-21 16:58:21 +0530

    added program to remove Kth node from LL

---
+++ LLdeletion3.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1)
+    {
+        data = data1;
+        next = NULL;
+    }
+};
+
+Node *arrtoLL(vector<int> arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+Node *removeK(Node *head, int k)
+{
+    if (head == NULL)
+        return head;
+    if (k == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int count = 0;
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp != NULL)
+    {
+        count++;
+        if (count == k)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {12, 5, 8, 7};
+    Node *head = arrtoLL(arr);
+    Node *temp1 = removeK(head, 3);
+    // Node *temp2 = removeK(head, 3);
+    print(temp1);
+    // print(temp2);
+}
