3a4362cda72c2c12b7b21583b16addd55ba1a733
avdhagam <davanisri@gmail.com>
2024-04-13 20:46:02 +0530

    added program to detect cycles in a linked list

---
+++ cycledetection.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1)
+    {
+        data = data1;
+        next = NULL;
+    }
+};
+
+Node *arrtoLL(vector<int> arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+bool cycledetection(Node *head)
+{
+    Node *fast = head;
+    Node *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4};
+    Node *head = arrtoLL(arr);
+    cout << cycledetection(head);
+}
