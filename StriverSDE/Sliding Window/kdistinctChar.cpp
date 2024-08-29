e64cc6cdd239a2ba6ddb6b7aae0a971a030af43b
avdhagam <davanisri@gmail.com>
2024-08-29 12:04:57 +0530

    added program to find the max substring with k distinct characters

---
+++ StriverSDE/Sliding Window/kdistinctChar.cpp
@@ -0,0 +1,25 @@
+#include <map>
+
+int kDistinctChars(int k, string &str)
+{
+    // Write your code here
+    int l = 0;
+    int r = 0;
+    int maxlen = 0;
+    map<int, int> mp;
+    while (r < str.size())
+    {
+        mp[str[r]]++;
+        while (mp.size() > k)
+        {
+            mp[str[l]]--;
+            if (mp[str[l]] == 0)
+                mp.erase(str[l]);
+            l++;
+        }
+        if (mp.size() <= k)
+            maxlen = max(maxlen, r - l + 1);
+        r++;
+    }
+    return maxlen;
+}
