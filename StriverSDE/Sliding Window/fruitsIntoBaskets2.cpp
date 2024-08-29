ecd88ccdcc88e3190ab8dbb220c9da6a78b3234e
avdhagam <davanisri@gmail.com>
2024-08-29 11:47:30 +0530

    added program to solve fruit baskets qn

---
+++ StriverSDE/Sliding Window/fruitsIntoBaskets2.cpp
@@ -0,0 +1,26 @@
+int totalFruits(vector<int> &arr)
+{
+    int maxlen = 0;
+    int l = 0;
+    int r = 0;
+    map<int, int> mp;
+    while (r < arr.size())
+    {
+        mp[arr[r]]++;
+        if (mp.size() > 2)
+        {
+            mp[arr[l]]--;
+            if (mp[arr[l]] == 0)
+            {
+                mp.erase(arr[l]);
+            }
+            l++;
+        }
+        if (mp.size() <= 2)
+        {
+            maxlen = max(maxlen, r - l + 1);
+        }
+        r++;
+    }
+    return maxlen;
+}
