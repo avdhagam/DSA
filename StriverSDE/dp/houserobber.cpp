be41ce461b251b7edc154043deb2e908c345955b
avdhagam <davanisri@gmail.com>
2024-08-30 20:13:06 +0530

    added program to solve house robber

---
+++ StriverSDE/dp/houserobber.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int f(int ind, vector<int> nums)
+    {
+        if (ind == 0)
+            return nums[ind];
+        int pick = INT_MIN;
+        if (ind > 1)
+        {
+            pick = f(ind - 2, nums) + nums[ind];
+        }
+        int notpick = f(ind - 1, nums) + 0;
+        return max(pick, notpick);
+    }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        return f(n - 1, nums);
+    }
+};
