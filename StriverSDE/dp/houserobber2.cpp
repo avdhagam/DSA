69901b3478d66b57e94468d7afd21db6a4832b41
avdhagam <davanisri@gmail.com>
2024-08-30 20:14:47 +0530

    added program to solve house robber using memoization

---
+++ StriverSDE/dp/houserobber2.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int f(int ind, vector<int> nums, vector<int> &dp)
+    {
+        if (ind == 0)
+            return nums[ind];
+        if (dp[ind] != -1)
+            return dp[ind];
+        int pick = INT_MIN;
+        if (ind > 1)
+        {
+            pick = f(ind - 2, nums, dp) + nums[ind];
+        }
+        int notpick = f(ind - 1, nums, dp) + 0;
+        return dp[ind] = max(pick, notpick);
+    }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return f(n - 1, nums, dp);
+    }
+};
