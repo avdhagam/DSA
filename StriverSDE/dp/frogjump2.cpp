61ace6e1a1f4c6a5ecb0b76d7cafc3585b659a08
avdhagam <davanisri@gmail.com>
2024-08-30 19:42:02 +0530

    added program to solve the frog jump qn using memoization

---
+++ StriverSDE/dp/frogjump2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+int f(int ind, vector<int> &heights, vector<int> &dp)
+{
+    if (ind == 0)
+        return 0;
+    if (dp[ind] != -1)
+        return dp[ind];
+
+    int left = f(ind - 1, heights, dp) + abs(heights[ind] - heights[ind - 1]);
+    int right = INT_MAX;
+    if (ind >= 2)
+    {
+        right = f(ind - 2, heights, dp) + abs(heights[ind] - heights[ind - 2]);
+    }
+    return dp[ind] = min(left, right);
+}
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int> dp(n, -1);
+    return f(n - 1, heights, dp);
+}
